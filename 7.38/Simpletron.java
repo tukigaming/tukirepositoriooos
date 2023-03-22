6b10de58c5e35734d4f1d8e29a8c6a27bba191da
tukigaming <axelgeronimo0@gmail.com>
2023-03-22 10:53:41 -0400

    amen

---
+++ 7.38/Simpletron.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class Simpletron {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] memory = new double[1000];
+        double acumulador = 0;
+        int contadorDeInstrucciones = 0;
+        int codigoDeOperacion = 0;
+        int operando = 100;
+        int registroDeInstruccion = 0;
+        
+        System.out.println("* Bienvenido a Simpletron! *");
+        System.out.println("* Por favor, introduzca en su programa una instruccion *");
+        System.out.println("* (o palabra de datos) a la vez. Yo le mostrare *");
+        System.out.println("* el numero de ubicacion y un signo de interrogacion (?) *");
+        System.out.println("* Entonces usted escribira la palabra para esa ubicacion. *");
+        System.out.println("* Teclee -9999 para dejar de introducir su programa. *");
+        
+        // Carga del programa en memoria
+        do {
+            System.out.printf("%02d ? ", contadorDeInstrucciones);
+            int instruccion = input.nextInt();
+            if (instruccion == -99999) {
+                break;
+            }
+            memory[contadorDeInstrucciones] = instruccion;
+            contadorDeInstrucciones++;
+            
+        } while (contadorDeInstrucciones < 1000);
+        
+        System.out.println("* Se completo la carga del programa *");
+        System.out.println("* Empieza la ejecucion del programa *");
+        
+        // Ejecución del programa en Simpletron
+        contadorDeInstrucciones = 0;
+
+        while (codigoDeOperacion != 43) {
+            registroDeInstruccion = (int) memory[contadorDeInstrucciones];
+            codigoDeOperacion = registroDeInstruccion / 100;
+            operando = registroDeInstruccion % 100;
+            
+            switch (codigoDeOperacion) {
+                case 10: // Leer
+                    System.out.print("Ingrese un entero: ");
+                    int numero = input.nextInt();
+                    memory[operando] = numero;
+                    break;
+                case 11: // Escribir
+                    System.out.printf("El contenido de la ubicacion %02d es %d%n", operando, memory[operando]);
+                    break;
+                case 20: // Cargar
+                    acumulador = memory[operando];
+                    break;
+                case 21: // Almacenar
+                    memory[operando] = acumulador;
+                    break;
+                case 30: // Sumar
+                    acumulador += memory[operando];
+                    if (acumulador > 9999 || acumulador < -99999) {
+                        System.out.println("* ERROR: Desbordamiento del acumulador *");
+                        System.exit(1);
+                    }
+                    break;
+                case 31: // Restar
+                    acumulador -= memory[operando];
+                    if (acumulador > 9999 || acumulador < -99999) {
+                        System.out.println("* ERROR: Desbordamiento del acumulador *");
+                        System.exit(1);
+                    }
+                    break;
+                case 32: // Dividir
+                    if (memory[operando] == 0) {
+                        System.out.println("* ERROR: Division por cero *");
+                        System.exit(1);
+                    }
+                    acumulador /= memory[operando];
+                    break;
+                    case 33: // residuo
+                    if (memory[operando] == 0) {
+                        System.out.println("* ERROR: Division por cero *");
+                        System.exit(1);
+                    }
+                    acumulador %= memory[operando];
+                    break;
+
+                    case 14:
+                    double base,expo,resut;
+                    base=acumulador;
+                    expo=memory[operando];
+                    resut=Math.pow(base,expo);
+                    acumulador=(int )resut;
+                    break; 
+
+                    case 15:
+                    int numeroHex = 0x1F;
+                    base=acumulador;
+                    expo=memory[operando];
+                    System.out.println(numeroHex);
+                    break;
+
+                    case 16:
+                    base=acumulador;
+                    expo=memory[operando];
+                    System.out.println("\n");
+                    break;
+
+                    case 17:
+                    base=acumulador;
+                    expo=memory[operando];
+
+
+
+
+
+            }
+            contadorDeInstrucciones++;
+        } 
+    }
+} 
